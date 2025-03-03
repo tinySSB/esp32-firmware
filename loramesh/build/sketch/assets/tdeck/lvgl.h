5d0ebb70032afcfff15344572572ca6997c14d25
mixmix <mix@protozoa.nz>
2025-03-03 17:06:54 +1300

    delete loramesh/build

--- loramesh/build/sketch/assets/tdeck/lvgl.h
+++
@@ -1,4 +0,0 @@
-#line 1 "/home/nanomonkey/git/tinySSB/esp32/loramesh/assets/tdeck/lvgl.h"
-// #ifdef TINYSSB_BOARD_TDECK
-# include <lvgl.h>
-// #endif
--- loramesh/build/sketch/loramesh.ino.cpp
+++
@@ -1,6 +0,0 @@
-#include <Arduino.h>
-#line 1 "/home/nanomonkey/git/tinySSB/esp32/loramesh/loramesh.ino"
-// loramesh.ino
-
-#include "loramesh.h" // just for having C-syntax highlighting in that file
-
